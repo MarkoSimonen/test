ffb1d935a85a2de7411e3ba57b762df16796d010
Marko Simonen <marko@simonen@hiq.fi>
2018-03-15 10:15:31 +0200

    first version

---
+++ test.tests/TestTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace test.tests
+{
+    [TestFixture]
+    public class TestTests
+    {
+        [Test]
+        public void VerifyTest()
+        {
+            var result = SumTest.Sum(3, 4);
+            Assert.AreEqual(result, 7);
+        }
+
+
+    }
+}
---
+++ test/SumTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    public class SumTest
+    {
+
+        public static int Sum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
